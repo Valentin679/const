10d06b25211aa071293c9a5d079c9a51c1f99ee0
Valentin679 <67257342+Valentin679@users.noreply.github.com>
2024-05-10 17:48:39 +0300

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,80 @@
+const packing = {
+    candies: {
+        big_box: {
+            id: 1,
+            size: {
+                width: 200,
+                height: 200,
+                depth: 30,
+                places: 16
+            },
+            filler: false,
+            color: 'white',
+            src: '200x200x30-white-16.jpg'
+
+        },
+        small_box: {
+            id: 2,
+            size: {
+                width: 145,
+                height: 145,
+                depth: 35,
+                places: 9
+            },
+            filler: false,
+            color: 'pink',
+            src: '145x145x35-pink-9.jgp'
+
+        },
+        small_choc_box: {
+            id: 3,
+            size: {
+                width: 177,
+                height: 177,
+                depth: 38,
+                places: 9
+            },
+            filler: false,
+            color: 'red',
+            src: '177x177x38-red-8+1.jpg'
+        },
+    },
+    chocolate: {
+        box_1: {
+            id: 1,
+            size: {
+                width: 150,
+                height: 150,
+                depth: 40,
+                places: 0
+            },
+            filler: true,
+            color: 'white',
+            src: '150x150x40-white.jpg'
+        },
+        box_2: {
+            id: 2,
+            size: {
+                width: 200,
+                height: 200,
+                depth: 35,
+                places: 0
+            },
+            filler: true,
+            color: 'white',
+            src: '200x200x35-white.jpg'
+        },
+        box_3: {
+            id: 3,
+            size: {
+                width: 250,
+                height: 210,
+                depth: 40,
+                places: 0
+            },
+            filler: true,
+            color: 'white',
+            src: '250x210x40-white.jpg'
+        },
+    }
+}
